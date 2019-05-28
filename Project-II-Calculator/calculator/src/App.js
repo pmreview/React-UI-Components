bb2ff27125fca9db7deb1d589e3fad3d619dc6e9
JaySebagh <jaysebagh@gmail.com>
2019-05-28 12:04:22 -0700

    appjs and css

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator-container">
+        <CalculatorDisplay buttonStyle="display" text={0}/>
+      <div className="buttons">
+        <div className="numbers">
+          <ActionButton buttonStyle={"action"} text={"clear"} />
+            <NumberButton buttonStyle={"number"} text={"7"} />
+            <NumberButton buttonStyle={"number"} text={"8"} />
+            <NumberButton buttonStyle={"number"} text={"9"} />
+            <NumberButton buttonStyle={"number"} text={"4"} />
+            <NumberButton buttonStyle={"number"} text={"5"} />
+            <NumberButton buttonStyle={"number"} text={"6"} />
+            <NumberButton buttonStyle={"number"} text={"1"} />
+            <NumberButton buttonStyle={"number"} text={"2"} />
+            <NumberButton buttonStyle={"number"} text={"3"} />
+          <ActionButton buttonStyle={"action"} text={"0"} />
+        </div>
+        <div className="operators">
+          <NumberButton buttonStyle={"operator"} text="/" />
+          <NumberButton buttonStyle={"operator"} text="X" />
+          <NumberButton buttonStyle={"operator"} text="-" />
+          <NumberButton buttonStyle={"operator"} text="+" />
+          <NumberButton buttonStyle={"operator"} text="=" />
+        </div>
+      </div>  
     </div>
   );
 };
